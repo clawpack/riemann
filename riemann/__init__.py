9451df8d1ecb2cd3893e19765a123070b775c111
David Ketcheson <dketch@gmail.com>
2011-05-18 11:56:26 +0300

    Add nonlinear elasticity python solver.

--- riemann/__init__.py
+++ riemann/__init__.py
@@ -8,7 +8,8 @@ Copyright (c) 2008 University of Washington. All rights reserved.
 """
 
 rp_solver_list_1d = ['advection','acoustics','burgers','shallow_roe',
-                  'shallow_hll','shallow_exact','euler_roe','vc_advection']
+                  'shallow_hll','shallow_exact','euler_roe','vc_advection',
+                  'nonlinear_elasticity']
 rp_solver_list_2d = []
 rp_solver_list_3d = []
 
@@ -19,3 +20,4 @@ from rp_acoustics import rp_acoustics_1d
 from rp_burgers import rp_burgers_1d
 from rp_shallow import rp_shallow_roe_1d, rp_shallow_hll_1d, rp_shallow_exact_1d
 from rp_euler import rp_euler_roe_1d
+from rp_nonlinear_elasticity import rp_nel_1d
