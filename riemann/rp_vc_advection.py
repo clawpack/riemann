7f3232c869bfe1063e24ef44618f65bd8afb2c2d
David Ketcheson <dketch@gmail.com>
2011-08-24 11:22:20 +0300

    Riemann solver for color equation.

--- riemann/rp_vc_advection.py
+++ riemann/rp_vc_advection.py
@@ -5,14 +5,9 @@ Variable coefficient 1D advection Riemann solver
 
 .. math::
     q_t + u(x) q_x = 0.
+
+Note that this is the color equation, not the conservative advection equation.
 """
-# ============================================================================
-#      Copyright (C) 2008 Kyle T. Mandli <mandli@amath.washington.edu>
-#
-#  Distributed under the terms of the Berkeley Software Distribution (BSD) 
-#  license
-#                     http://www.opensource.org/licenses/
-# ============================================================================
 
 import numpy as np
 
@@ -42,13 +37,8 @@ def rp_vc_advection_1d(q_l,q_r,aux_l,aux_r,aux_global):
     
     wave[0,0,:] = q_r[0,:] - q_l[0,:]
 
-    #print "test: aux_l size", aux_l.shape
-    #print "test: s size", s[0,:].shape
-
-    # Amal: it was s[0,:] = aux_l, you need to figure it out
     s[0,:] = aux_l[0,:]
 
-    # Amal: it was  aux_l in both lines, you need to figure it out
     apdq[0,:] = (aux_l[0,:]>0)*s[0,:] * wave[0,0,:]
     amdq[0,:] = (aux_l[0,:]<0)*s[0,:] * wave[0,0,:]
 
