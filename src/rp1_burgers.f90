d5ba5ce618e040ea0974f7ca154166a1f8eca94d
David Ketcheson <dketch@gmail.com>
2012-01-29 21:37:08 +0300

    Non-working attempt to dynamically select the Riemann solver.

--- src/rp1_burgers.f90
+++ src/rp1_burgers.f90
@@ -1,5 +1,5 @@
 ! =============================================================================
-	subroutine rp1(maxmx,meqn,mwaves,mbc,mx,ql,qr,auxl,auxr,wave,s,amdq,apdq,num_aux)
+subroutine rp1(maxmx,meqn,mwaves,mbc,mx,ql,qr,auxl,auxr,wave,s,amdq,apdq,num_aux)
 ! =============================================================================
 !
 ! Riemann problems for the 1D Burgers' equation with entropy fix for 
@@ -7,10 +7,10 @@
 ! R. J. LeVeque.
 
     
-    implicit none 
-	
-	integer :: maxmx, meqn, mwaves, mbc, mx
-	
+    implicit double precision (a-h,o-z)
+        
+    integer :: maxmx, meqn, mwaves, mbc, mx
+        
     double precision :: ql(meqn,1-mbc:maxmx+mbc)
     double precision :: qr(meqn,1-mbc:maxmx+mbc)
     double precision :: s(mwaves, 1-mbc:maxmx+mbc)
@@ -21,17 +21,17 @@
     integer :: i
 
     do i=2-mbc,mx+mbc
-		wave(1,1,i) = ql(1,i) - qr(1,i-1)
+        wave(1,1,i) = ql(1,i) - qr(1,i-1)
         s(1,i) = 0.5d0 * (qr(1,i-1) + ql(1,i))
 
         amdq(i,1) = dmin1(s(1,i), 0.d0) * wave(1,1,i)
         apdq(i,1) = dmax1(s(1,i), 0.d0) * wave(1,1,i)
 
         if (ql(1,i).gt.0.d0 .and. qr(1,i-1).lt.0.d0) then
-        	amdq(1,i) = - 1.d0/2.d0 * qr(1,i-1)**2
+            amdq(1,i) = - 1.d0/2.d0 * qr(1,i-1)**2
             apdq(1,i) =   1.d0/2.d0 * ql(1,i)**2
         endif
-	enddo
+    enddo
 
     return
     end subroutine
