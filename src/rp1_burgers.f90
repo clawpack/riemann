dd169d0f6734cb64144a6edbce4874aaaebe6855
Randy LeVeque <rjl@ned>
2013-10-20 17:42:03 -0700

    Added efix=.true. parameter to rp1_burgers.f90

--- src/rp1_burgers.f90
+++ src/rp1_burgers.f90
@@ -17,8 +17,11 @@ subroutine rp1(maxmx,meqn,mwaves,maux,mbc,mx,ql,qr,auxl,auxr,wave,s,amdq,apdq)
     double precision :: wave(meqn, mwaves, 1-mbc:maxmx+mbc)
     double precision :: amdq(meqn, 1-mbc:maxmx+mbc)
     double precision :: apdq(meqn, 1-mbc:maxmx+mbc)
-    
     integer :: i
+    logical :: efix
+ 
+    efix = .true.   !# Compute correct flux for transonic rarefactions
+ 
 
     do i=2-mbc,mx+mbc
         wave(1,1,i) = ql(1,i) - qr(1,i-1)
@@ -27,9 +30,11 @@ subroutine rp1(maxmx,meqn,mwaves,maux,mbc,mx,ql,qr,auxl,auxr,wave,s,amdq,apdq)
         amdq(1,i) = dmin1(s(1,i), 0.d0) * wave(1,1,i)
         apdq(1,i) = dmax1(s(1,i), 0.d0) * wave(1,1,i)
 
-        if (ql(1,i).gt.0.d0 .and. qr(1,i-1).lt.0.d0) then
-            amdq(1,i) = - 1.d0/2.d0 * qr(1,i-1)**2
-            apdq(1,i) =   1.d0/2.d0 * ql(1,i)**2
+	    if (efix) then
+            if (ql(1,i).gt.0.d0 .and. qr(1,i-1).lt.0.d0) then
+                amdq(1,i) = - 1.d0/2.d0 * qr(1,i-1)**2
+                apdq(1,i) =   1.d0/2.d0 * ql(1,i)**2
+            endif
         endif
     enddo
 
