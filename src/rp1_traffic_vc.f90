738f951d606b038829288ff9648c813e14c842f8
David Ketcheson <dketch@gmail.com>
2017-02-14 18:00:58 -0800

    Switch back to using f-waves in variable-speed traffic solvers.

--- src/rp1_traffic_vc.f90
+++ src/rp1_traffic_vc.f90
@@ -19,7 +19,7 @@
 ! of the Riemann solver API.
 
 subroutine rp1(maxm,num_eqn,num_waves,num_aux,num_ghost,num_cells, &
-               ql,qr,auxl,auxr,wave,s,amdq,apdq)
+               ql,qr,auxl,auxr,fwave,s,amdq,apdq)
 
     implicit none
     ! Inputs
@@ -28,11 +28,11 @@ subroutine rp1(maxm,num_eqn,num_waves,num_aux,num_ghost,num_cells, &
     double precision, intent(in), dimension(num_aux, 1-num_ghost:maxm+num_ghost) :: auxl,auxr
     ! Outputs
     double precision, intent(out) :: s(num_waves,1-num_ghost:num_cells+num_ghost)
-    double precision, intent(out) :: wave(num_eqn,num_waves,1-num_ghost:num_cells+num_ghost)
+    double precision, intent(out) :: fwave(num_eqn,num_waves,1-num_ghost:num_cells+num_ghost)
     double precision, intent(out), dimension(num_eqn, 1-num_ghost:maxm+num_ghost) :: amdq,apdq
     ! Locals
     integer :: i
-    double precision :: fim1, fi, sim1, si, f0, fluxdiff
+    double precision :: fim1, fi, sim1, si, f0
 
     do i = 2-num_ghost, num_cells+num_ghost
         ! Compute the wave and speed, and fluctuations
@@ -40,8 +40,7 @@ subroutine rp1(maxm,num_eqn,num_waves,num_aux,num_ghost,num_cells, &
         ! compute flux in each cell and flux difference:
         fim1 = auxr(1,i-1)*qr(1,i-1)*(1.d0 - qr(1,i-1))
         fi   = auxl(1,i  )*ql(1,i  )*(1.d0 - ql(1,i  ))
-        fluxdiff = fi - fim1
-        wave(1,1,i) = ql(1,i) - qr(1,i-1)
+        fwave(1,1,i) = fi - fim1
 
         ! compute characteristic speed in each cell:
         sim1 = auxr(1,i-1)*(1.d0 - 2.d0*qr(1,i-1))
@@ -50,13 +49,13 @@ subroutine rp1(maxm,num_eqn,num_waves,num_aux,num_ghost,num_cells, &
         if (sim1 .lt. 0.d0 .and. si .le. 0.d0) then
             ! left-going
             s(1,i) = sim1
-            amdq(1,i) = fluxdiff
+            amdq(1,i) = fwave(1,1,i)
             apdq(1,i) = 0.d0
         else if (sim1 .ge. 0.d0 .and. si .gt. 0.d0) then
             ! right-going
             s(1,i) = si
             amdq(1,i) = 0.d0
-            apdq(1,i) = fluxdiff
+            apdq(1,i) = fwave(1,1,i)
         else if (sim1 .lt. 0.d0 .and. si .gt. 0.d0) then
             ! transonic rarefaction
             ! entropy fix:  (perhaps doesn't work for all cases!!!)
@@ -72,14 +71,14 @@ subroutine rp1(maxm,num_eqn,num_waves,num_aux,num_ghost,num_cells, &
             ! transonic shock
             s(1,i) = 0.5d0*(sim1 + si)
             if (s(1,i) .lt. 0.d0) then
-                amdq(1,i) = fluxdiff
+                amdq(1,i) = fwave(1,1,i)
                 apdq(1,i) = 0.d0
             else if (s(1,i) .gt. 0.d0) then
                 amdq(1,i) = 0.d0
-                apdq(1,i) = fluxdiff
+                apdq(1,i) = fwave(1,1,i)
             else
-                amdq(1,i) = 0.5d0 * fluxdiff
-                apdq(1,i) = 0.5d0 * fluxdiff
+                amdq(1,i) = 0.5d0 * fwave(1,1,i)
+                apdq(1,i) = 0.5d0 * fwave(1,1,i)
             endif
         endif
 
