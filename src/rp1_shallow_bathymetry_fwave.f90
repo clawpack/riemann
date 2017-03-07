98c3231b08dd1f1a59a1780aa0fac2615979401c
Kyle Mandli <kyle.mandli@gmail.com>
2017-03-07 17:49:38 -0500

    Reconcile differences between python and fortran RP for swe
    Also corrected some bugs that were found and added more problem parameters
    to the Python version.

--- src/rp1_shallow_bathymetry_fwave.f90
+++ src/rp1_shallow_bathymetry_fwave.f90
@@ -34,86 +34,80 @@ subroutine rp1(maxm,num_eqn,num_waves,num_aux,num_ghost,num_cells,ql,qr,auxl,aux
     real(kind=8), intent(out) :: amdq(num_eqn, 1-num_ghost:maxm+num_ghost)
     real(kind=8), intent(out) :: apdq(num_eqn, 1-num_ghost:maxm+num_ghost)
     
-    real(kind=8) :: grav
-    common /cparam/ grav
+    real(kind=8) :: grav, dry_tolerance
+    common /cparam/ grav, dry_tolerance
     
-    real(kind=8) :: hl, ul, hr, ur, hbar, uhat, chat, bl, br
-    real(kind=8) :: R(2,2), L(2,2)
-    real(kind=8) :: fluxdiff(2), beta(2)
+    real(kind=8) :: hl, ul, hr, ur, hbar, uhat, chat, bl, br, phil, phir
+    real(kind=8) :: R(2,2), fluxdiff(2), beta(2)
     
-    integer :: i, j, k
-    
+    integer :: i, k
+
+    amdq = 0.d0
+    apdq = 0.d0
     
     do i=2-num_ghost,num_cells+num_ghost
         ! # Left states
-        hl = qr(1,i-1)
-        ul = qr(2,i-1)/hl
-        bl = auxr(1,i-1)
+        hl = qr(1, i - 1)
+        if (hl > dry_tolerance) then
+            ul = qr(2, i - 1) / hl
+            phil = qr(1, i - 1) * ul**2 + 0.5d0 * grav * qr(1, i - 1)**2
+        else
+            ul = 0.d0
+            phil = 0.d0
+        end if
+        bl = auxr(1, i - 1)
         
         ! # Right states
-        hr = ql(1,i)
-        ur = ql(2,i)/hr
-        br = auxl(1,i)
+        hr = ql(1, i)
+        if (hr > dry_tolerance) then
+            ur = ql(2, i)/hr
+            phir = ql(1, i) * ur**2 + 0.5d0 * grav * ql(1, i)**2
+        else
+            ur = 0.d0
+            phir = 0.d0
+        end if
+        br = auxl(1, i)
         
         ! # Roe average states
-        hbar = 0.5*(hr+hl)
-        uhat = (dsqrt(hl)*ul + dsqrt(hr)*ur)/(dsqrt(hl)+dsqrt(hr))
-        chat = dsqrt(grav*hbar)
+        hbar = 0.5 * (hr + hl)
+        uhat = (sqrt(hl) * ul + sqrt(hr) * ur) / (sqrt(hl) + sqrt(hr))
+        chat = sqrt(grav * hbar)
         
         ! # Flux differences
-        fluxdiff(1) = (hr*ur) - (hl*ul)
-        fluxdiff(2) = (hr*ur*ur + 0.5*grav*hr**2) - (hl*ul*ul + 0.5*grav*hl**2) + grav*hbar*(br-bl)
+        fluxdiff(1) = (hr * ur) - (hl * ul)
+        fluxdiff(2) = phir - phil + grav * hbar * (br - bl)
         
         ! # Wave speeds
-        s(1,i) = uhat-chat
-        s(2,i) = uhat+chat
+        s(1,i) = min(ul - sqrt(grav * hl), uhat - chat)
+        s(2,i) = max(ur + sqrt(grav * hr), uhat + chat)
         
         ! # Right eigenvectors (column)
         R(1,1) = 1.d0
-        R(2,1) = uhat-chat
+        R(2,1) = s(1, i)
         
         R(1,2) = 1.d0
-        R(2,2) = uhat+chat
+        R(2,2) = s(2, i)
     
-        ! # Left eigenvectors (rows)
-        L(1,1) = (chat+uhat)/(2.d0*chat)
-        L(2,1) = (chat-uhat)/(2.d0*chat)
-        
-        L(1,2) = -1d0/(2.d0*chat)
-        L(2,2) = 1.d0/(2.d0*chat)
-        
-        ! # Coefficients beta
-        do j=1,num_waves
-            beta(j) = 0.d0
-            do k=1,num_eqn
-                beta(j) = beta(j) + L(j,k)*fluxdiff(k)
-            enddo
-        enddo
+        ! Wave strengths
+        beta(1) = (s(2, i) * fluxdiff(1) - fluxdiff(2)) / (s(2, i) - s(1, i))
+        beta(2) = (fluxdiff(2) - s(1, i) * fluxdiff(1)) / (s(2, i) - s(1, i))
 
         ! # Flux waves
-        do j=1,num_eqn
-            do k=1,num_waves
-                fwave(j,k,i) = beta(k)*R(j,k)
-            enddo
+        do k=1,num_waves
+            fwave(:,k,i) = beta(k) * R(:,k)
         enddo
         
-        
         ! # Fluctuations
-        do j=1,num_eqn
-            amdq(j,i) = 0.d0
-            apdq(j,i) = 0.d0
-            do k=1,num_eqn
-                if (s(k,i) .lt. 0.d0) then
-                    amdq(j,i) = amdq(j,i) + fwave(j,k,i)
-                elseif (s(k,i) .ge. 0d0) then
-                    apdq(j,i) = apdq(j,i) + fwave(j,k,i)
-                else
-                    amdq(j,i) = amdq(j,i) + 0.5*fwave(j,k,i)
-                    apdq(j,i) = apdq(j,i) + 0.5*fwave(j,k,i)
-                endif
-            enddo
+        do k=1, num_waves
+            if (s(k, i) < 0.d0) then
+                amdq(:, i) = amdq(:, i) + fwave(:, k, i)
+            else if (s(k, i) > 0.d0) then
+                apdq(:, i) = apdq(:, i) + fwave(:, k, i)
+            else
+                amdq(:, i) = amdq(:, i) + 0.5d0 * fwave(:, k, i)
+                apdq(:, i) = apdq(:, i) + 0.5d0 * fwave(:, k, i)
+            end if
         enddo
     enddo
     
-    return
 end subroutine rp1
