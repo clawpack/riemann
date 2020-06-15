9932d1322b1dbb6e945049757275bb90ab5e5dd2
Randy LeVeque <rjl@uw.edu>
2020-06-15 10:50:15 -0700

    fix bug in rpt2_geoclaw and switch to using Jacobian from left or right state, not Roe average

--- src/rpt2_geoclaw.f
+++ src/rpt2_geoclaw.f
@@ -7,10 +7,18 @@
 
       implicit none
 !
-!     # Riemann solver in the transverse direction using an einfeldt
-!     Jacobian.
+!     Riemann solver in the transverse direction using 
+!     Jacobian matrix from left cell (if imp==1) or right cell (if imp==2).
+!
+!     Note this has been modified from the version used in v5.7.0 and
+!     earlier, where Roe averages based on ql and qr were used, which
+!     is not correct.  In addition:
+!      - a bug in the second component of the eigenvectors was fixed.
+!      - when s(2) is close to zero this component of flux difference
+!        is split equally between bmasdq and bpasdq to improve symmetry.
+!   
 
-!-----------------------last modified 1/10/05----------------------
+!-----------------------last modified June 2020 ----------------------
 
       integer ixy,maxm,meqn,maux,mwaves,mbc,mx,imp
 
@@ -23,9 +31,9 @@
       double precision  aux2(maux,1-mbc:maxm+mbc)
       double precision  aux3(maux,1-mbc:maxm+mbc)
 
-      double precision  s(3)
-      double precision  r(3,3)
-      double precision  beta(3)
+      double precision  s(mwaves)
+      double precision  r(meqn,mwaves)
+      double precision  beta(mwaves)
       double precision  abs_tol
       double precision  hl,hr,hul,hur,hvl,hvr,vl,vr,ul,ur,bl,br
       double precision  uhat,vhat,hhat,roe1,roe3,s1,s2,s3,s1l,s3r
@@ -33,7 +41,7 @@
       double precision  dxdcm,dxdcp,topo1,topo3,eta
 
       integer i,m,mw,mu,mv
-
+      
       abs_tol=tol
 
       if (ixy.eq.1) then
@@ -118,40 +126,51 @@ c            s2 = 0.5d0*(s1+s3)
       endif
 
 c=====Determine some speeds necessary for the Jacobian=================
-            vhat=(vr*dsqrt(hr))/(dsqrt(hr)+dsqrt(hl)) +
-     &        (vl*dsqrt(hl))/(dsqrt(hr)+dsqrt(hl))
 
-            uhat=(ur*dsqrt(hr))/(dsqrt(hr)+dsqrt(hl)) +
-     &        (ul*dsqrt(hl))/(dsqrt(hr)+dsqrt(hl))
-            hhat=(hr+hl)/2.d0
+            
+            ! In v5.7.0 and prior versions,
+            ! we used left right states to define Roe averages,
+            ! which is consistent with those used in rpn2.
+            ! But now we are computing upgoing, downgoing waves either in
+            ! cell on left (if imp==1) or on right (if imp==2) so we
+            ! should possibly use q values in cells above/below,
+            ! but these aren't available (only aux values).
+            ! At any rate, there is no clear justification for using cells
+            ! on the other side of the normal-solve interface.
 
-            roe1=vhat-dsqrt(g*hhat)
-            roe3=vhat+dsqrt(g*hhat)
+            ! Modified to use left or right state alone in defining
+            ! Jacobian, based on imp:
 
-            s1l=vl-dsqrt(g*hl)
-            s3r=vr+dsqrt(g*hr)
+            if (imp == 1) then
+                ! asdq is leftgoing, use q from cell i-1:
+                s(1) = vl-dsqrt(g*hl)
+                s(2) = vl
+                s(3) = vl+dsqrt(g*hl)
+                uhat = ul
+                hhat = hl
+            else
+                ! asdq is rightgoing, use q from cell i:
+                s(1) = vr-dsqrt(g*hr)
+                s(2) = vr
+                s(3) = vr+dsqrt(g*hr)
+                uhat = ur
+                hhat = hr
+            endif
 
-            s1=dmin1(roe1,s1l)
-            s3=dmax1(roe3,s3r)
-
-            s2=0.5d0*(s1+s3)
-
-           s(1)=s1
-           s(2)=s2
-           s(3)=s3
 c=======================Determine asdq decomposition (beta)============
          delf1=asdq(1,i)
          delf2=asdq(mu,i)
          delf3=asdq(mv, i)
 
+         ! fixed bug in beta(2): uhat in place of s(2)
          beta(1) = (s3*delf1/(s3-s1))-(delf3/(s3-s1))
-         beta(2) = -s2*delf1 + delf2
+         beta(2) = -uhat*delf1 + delf2
          beta(3) = (delf3/(s3-s1))-(s1*delf1/(s3-s1))
 c======================End =================================================
 
 c=====================Set-up eigenvectors===================================
          r(1,1) = 1.d0
-         r(2,1) = s2
+         r(2,1) = uhat    ! fixed bug, uhat not s2
          r(3,1) = s1
 
          r(1,2) = 0.d0
@@ -159,20 +178,35 @@ c=====================Set-up eigenvectors===================================
          r(3,2) = 0.d0
 
          r(1,3) = 1.d0
-         r(2,3) = s2
+         r(2,3) = uhat    ! fixed bug, uhat not s2
          r(3,3) = s3
 c============================================================================
 90      continue
 c============= compute fluctuations==========================================
 
-               bmasdq(1,i)=0.0d0
-               bpasdq(1,i)=0.0d0
-               bmasdq(2,i)=0.0d0
-               bpasdq(2,i)=0.0d0
-               bmasdq(3,i)=0.0d0
-               bpasdq(3,i)=0.0d0
+          
+         ! initialize all components to 0:
+         bmasdq(:,:) = 0.d0
+         bpasdq(:,:) = 0.d0
+               
             do  mw=1,3
-               if (s(mw).lt.0.d0) then
+               if ((abs(s(mw)) > 0.d0) .and. 
+     &             (abs(s(mw)) < 0.001d0*dsqrt(g*hhat))) then
+                 ! split correction symmetrically if nearly zero
+                 ! Note wave drops out if s(mw)==0 exactly, so no need to split
+                 bmasdq(1,i) =bmasdq(1,i) +
+     &                        0.5d0*dxdcm*s(mw)*beta(mw)*r(1,mw)
+                 bmasdq(mu,i)=bmasdq(mu,i)+ 
+     &                        0.5d0*dxdcm*s(mw)*beta(mw)*r(2,mw)
+                 bmasdq(mv,i)=bmasdq(mv,i)+ 
+     &                        0.5d0*dxdcm*s(mw)*beta(mw)*r(3,mw)
+                 bpasdq(1,i) =bpasdq(1,i) + 
+     &                        0.5d0*dxdcp*s(mw)*beta(mw)*r(1,mw)
+                 bpasdq(mu,i)=bpasdq(mu,i)+ 
+     &                        0.5d0*dxdcp*s(mw)*beta(mw)*r(2,mw)
+                 bpasdq(mv,i)=bpasdq(mv,i)+ 
+     &                        0.5d0*dxdcp*s(mw)*beta(mw)*r(3,mw)
+               elseif (s(mw).lt.0.d0) then
                  bmasdq(1,i) =bmasdq(1,i) + dxdcm*s(mw)*beta(mw)*r(1,mw)
                  bmasdq(mu,i)=bmasdq(mu,i)+ dxdcm*s(mw)*beta(mw)*r(2,mw)
                  bmasdq(mv,i)=bmasdq(mv,i)+ dxdcm*s(mw)*beta(mw)*r(3,mw)
