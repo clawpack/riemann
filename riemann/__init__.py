0724f8cdf61be50e0980a6e8ad0de5d565d29dbc
David Ketcheson <dketch@gmail.com>
2026-02-09 15:08:01 +0300

    Add constants file for mapped-grid 3D Euler.
    Thanks to @zezl7 for catching this!

--- riemann/__init__.py
+++ riemann/__init__.py
@@ -50,7 +50,7 @@ from . import vc_acoustics_3D_constants
 from . import euler_3D_constants
 from . import burgers_3D_constants
 from . import vc_advection_3D_constants
-from . import euler_mapgrid_3D
+from . import euler_mapgrid_3D_constants
 
 
 try:
